ac41139c15d5fc79dd598d085d9f959cfa667f2e
towa.sasaki <towa.sasaki@rakuten.com>
2018-06-15 01:54:12 +0000

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>SASORICH</title>
+    </head>
+
+    <body>
+        @yield('content')
+        @include('layouts.footer')
+    </body>
+</html>
